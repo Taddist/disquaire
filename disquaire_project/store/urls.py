09fcba30ea6a1d039d44268036e2789d7b26454b
Afaf Taddist <afaf.taddist71@gmail.com>
2017-09-09 14:16:05 +0100

    add vue

--- disquaire_project/store/urls.py
+++ disquaire_project/store/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views  # import views so we can use the in urls
 
 urlpatterns = [
-    url(r'^$', views.listing)
+    url(r'^$', views.listing),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail),
 ]
