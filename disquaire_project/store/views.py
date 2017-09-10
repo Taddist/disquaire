1541c8f552b4fb094e3324be79b0867d279343d3
Afaf Taddist <afaf.taddist71@gmail.com>
2017-09-10 13:50:36 +0100

    Pagination

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Album, Artist, Contact, Booking
 # Create your views here.
@@ -12,9 +13,23 @@ def index(request):
     return render(request, 'store/index.html', context)
 
 def listing(request):
-    albums = Album.objects.filter(available=True)
+    albums_list = Album.objects.filter(available=True)
+    # Slice pages
+    paginator = Paginator(albums_list, 9)
+    # Get current page number
+    page = request.GET.get('page')
+    # Return only this page albums and not others
+    try:
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        albums = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages)
     context = {
-        'albums': albums
+        'albums': albums,
+        'paginate': True
     }
     return render(request, 'store/listing.html', context)
 
